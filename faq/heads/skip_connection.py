f77bbb5870531049a1eacc6ac93487968c9163d5
George Panchuk <george.panchuk@qdrant.tech>
2022-02-09 16:28:58 +0300

    new: add models for experiments

---
+++ faq/heads/skip_connection.py
@@ -0,0 +1,21 @@
+import torch
+
+from quaterion_models.heads import EncoderHead
+
+
+class SkipConnectionHead(EncoderHead):
+
+    def __init__(self, input_embedding_size: int, output_embedding_size):
+        super().__init__(input_embedding_size)
+        self.output_embeddings_size = output_embedding_size
+        self.fc = torch.nn.Linear(input_embedding_size, output_embedding_size)
+
+    def output_size(self) -> int:
+        return self.output_embeddings_size
+
+    def forward(self, input_vectors: torch.Tensor) -> torch.Tensor:
+        """
+        :param input_vectors: shape: (batch_size * vector_size)
+        :return: (batch_size * vector_size)
+        """
+        return self.fc(input_vectors) + input_vectors
---
+++ faq/pipelines/__init__.py
@@ -0,0 +1,4 @@
+from .gated import GatedModelPipeline
+from .projector import ProjectorModelPipeline
+from .skip_connection import SkipConnectionModelPipeline
+from .stacked import StackedModelPipeline
